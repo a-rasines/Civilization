972b24972fb593f44a24880e274259e30419dfa8
a-rasines <alberto.rasines@opendeusto.es>
2022-05-21 18:37:29 +0200

    Fase 1 del mapa

--- MapWindow.cpp
+++ MapWindow.cpp
@@ -10,9 +10,11 @@
 #include "MapHolder.h"
 float MapWindow::x;
 float MapWindow::y;
+int MapWindow::zoom;
 MapWindow::MapWindow() {
 	x = 0;
 	y = 0;
+	zoom = 5;
 }
 //int MapWindow::x;
 //int MapWindow::y;
@@ -22,33 +24,36 @@ void MapWindow::start(){
 	setResizable(true);
 }
 void MapWindow::update(){
+	mapView.clear();
+	for(Cell cell : activeCells){
+		sf::Texture tex;
+		std::string file = "resources/" + (std::string)cell.file;
+		tex.loadFromFile(file, cell.texData);
+		sf::RectangleShape rect;
+		rect.setTexture(&tex, false);
+		rect.setSize(sf::Vector2f(cell.sizeX, cell.sizeY));
+		rect.setPosition(sf::Vector2f(cell.posX, cell.posY));
+		mapView.draw(rect);
+	}
+	mapView.display();
+
 }
-int count = 0;
 void MapWindow::reposition(float x, float y){
 	WindowManager::Dimension size = Window::manager->getWindowSize();
 	activeCells.clear();
-	sf::Texture tex;
-	count++;
 	y = -y;
 	for(TerrainType row[100] : foreground){
-		std::list<sf::RectangleShape> rowList = std::list<sf::RectangleShape>();
-		y += row[0].sizeY;
-		if(y >= size.y)break;
+		y += row[0].sizeY * zoom;
+		if(y >= size.y + row[0].sizeY)break;
 		else if(y<0)continue;
 		int cx = -x;
 		for(TerrainType cell : row){
-			cx += cell.sizeX;
-			if(cx >= size.x)break;
+			cx += cell.sizeX * zoom;
+			if(cx >= size.x + cell.sizeX)break;
 			else if(cx<0)continue;
-			std::string file = "resources/" + (std::string)cell.file;
-			tex.loadFromFile(file, sf::IntRect(cell.textureX, cell.textureY, cell.sizeX, cell.sizeY));
-			sf::RectangleShape rect(sf::Vector2f(cell.sizeX*zoom, cell.sizeY*zoom));
-			rect.setTexture(&tex, false);
-
+			activeCells.push_back((Cell){cx-cell.sizeX*zoom, y-cell.sizeY*zoom, cell.sizeX * zoom, cell.sizeY * zoom, cell.file, sf::IntRect(cell.textureX, cell.textureY, cell.sizeX, cell.sizeY)});
 		}
-		activeCells.push_back(rowList);
 	}
-	std::cout << count << "\n";
 }
 void MapWindow::onResize(int newWidth, int newHeight){
 	reposition(x, y);
--- MapWindow.h
+++ MapWindow.h
@@ -9,8 +9,10 @@
 #define MAPWINDOW_H_
 #include "WindowObjects/Window.h"
 #include <SFML/Graphics.hpp>
-#include <list>
+#include <vector>
+#include <string>
 #include "Sprite.h"
+#include"Ejemplo2.h"
 class MapWindow : public Window{
 public:
 	MapWindow();
@@ -19,13 +21,22 @@ public:
 	void onButtonPress(HWND button){};//Esta funcion se llama cuando un boton sea pulsado
 	void reposition(float x, float y);//Se llama para mover el mapa
 	void onResize(int newWidth, int newHeight);//Esta funcion se llama cuando la ventana cambia de tamaño
+	struct Cell{
+		int posX;
+		int posY;
+		int sizeX;
+		int sizeY;
+		std::string file;
+		sf::IntRect texData;
+
+	};
 	virtual ~MapWindow();
-	static int zoom = 1;
+	static int zoom;
 private:
 	sf::RenderWindow mapView;
 	static float x;
 	static float y;
-	std::list<std::list<sf::RectangleShape>> activeCells;
+	std::vector<Cell> activeCells;
 };
 
 #endif /* MAPWINDOW_H_ */
